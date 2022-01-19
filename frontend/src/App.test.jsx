72814202a64cbe54902229779fdf594ca2c3aa82
Ewelina W <ewelina.wystepek@gmail.com>
2022-01-19 21:31:05 +0100

    React Testing Library added

--- frontend/src/App.test.jsx
+++ frontend/src/App.test.jsx
@@ -1,5 +1,27 @@
+import React from 'react';
+import {render, fireEvent, waitFor, screen} from '@testing-library/react'
+import '@testing-library/jest-dom'
 import App from './App';
 
-test('adds 1 + 2 to equal 3', () => {
-    expect(1 + 2).toBe(3);
+describe('App', () => {
+    describe('button', () => {
+        it('has proper name', () => {
+            render(<App />);
+            const button = screen.getByRole('button');
+            expect(button).toHaveTextContent('Material UI')
+        });
+
+        it('not disabled', () => {
+            render(<App/>)
+            expect(screen.getByRole('button')).not.toBeDisabled();
+        })
+    })
+    
+    describe('heading', () => {
+        it('has a proper text', () => {
+            render(<App />);
+            const heading = screen.getByRole('heading');
+            expect(heading).toHaveTextContent('Hello React')
+        });
+    });
 });
