b432f680ff5440964ef829a6c041f2b5f9505ae8
Mateusz <matzaj101@gmail.com>
2022-01-26 23:54:48 +0100

    remove example test

--- frontend/cypress/integration/example/example.spec.js
+++
@@ -1,7 +0,0 @@
-describe('this is an exapmle', ()=> {
-    it('visit web app and find title', ()=>{
-        cy.visit('http://localhost:8080');
-        cy.get('h1')
-        cy.get('button').click();
-    })
-})
