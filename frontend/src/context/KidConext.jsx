341d6070a1536513d123152519a9aa16a4f39150
Marharyta Lesniak <sirachenko944@gmail.com>
2022-01-29 20:51:05 +0100

    Add states

--- frontend/src/context/KidConext.jsx
+++ frontend/src/context/KidConext.jsx
@@ -1,17 +1,23 @@
 import React, { createContext, useState } from "react";
-
 export const KidContext = createContext();
 
-export const KidProvider = ({ children }) => {
-  const [height, setHeight] = React.useState(0);
-  const [weight, setWeight] = React.useState(0);
-  const [bmi, setBmi] = React.useState(0);
-  const [gender, setGender] = React.useState("");
-  const [avatar, setAvatar] = React.useState();
-  const [image, setImage] = React.useState("");
+export function KidProvider({ children }) {
+  const [name, setName] = useState("");
+  const [birthday, setBirthday] = useState("");
+  const [height, setHeight] = useState(0);
+  const [weight, setWeight] = useState(0);
+  const [bmi, setBmi] = useState(0);
+  const [gender, setGender] = useState("");
+  const [avatar, setAvatar] = useState("");
+  const [image, setImage] = useState("");
+
   return (
     <KidContext.Provider
       value={{
+        name,
+        setName,
+        birthday,
+        setBirthday,
         height,
         setHeight,
         weight,
