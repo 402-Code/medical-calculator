22a962bcb9324368c408c8a5eea89c5593b9205e
unknown <ty@example.comaleksandra.tarnawska@coderscrew.pl>
2022-02-01 23:15:05 +0100

    drugfinder

---
+++ frontend/src/components/DrugFinder/DrugFinder.jsx
@@ -0,0 +1,56 @@
+import { useState } from "react";
+import "./DrugFinder.scss";
+import { Link } from "react-router-dom";
+import SearchDrug from "./SearchDrug";
+
+
+const SUBSTANCE = ["Ibuprofen", "Paracetamol"]
+const DRUG = ["Ibum Forte", "Ibufen", "Ibuprom", "Nurofen", "Paracetamol DOZ", "Pedicetamol", "Apap Junior"]
+
+const DrugFinder = () => {
+    const [sunstance, setSubstance] = useState("Ibuprofen", "Paracetamol")
+    const [drug, setDrug] = useState("")
+ 
+return (
+    <div className="drug-finder">
+    <form>
+        <label htmlFor="substance">
+            Substancja czynna
+            <select
+                id="substance"
+                value={sunstance}
+                onChange={(e) => {
+                    return setSubstance(e.target.value);
+                }}
+                onBlur={(e) => setSubstance(e.target.value)}
+            >
+            <option value=""></option>
+            {SUBSTANCE.map(substance => (
+                  <option value={substance} key={substance}>
+                      {substance}
+                  </option>
+                ))}
+            </select>
+        </label>
+        <label htmlFor="drug">
+            Lekarstwo
+            <select
+                id="drug"
+                value={drug}
+                onChange={(e) => setDrug(e.target.value)}
+                onBlur={(e) => setDrug(e.target.value)}
+        >
+            <option value=""></option>
+            {drug.map(drug => (
+                  <option value={drug} key={drug}>
+                      {drug}
+                  </option>
+                ))}           
+            </select>
+        </label>
+    </form>
+    </div>
+    );
+}};
+
+export default DrugFinder;
---
+++ frontend/src/components/DrugFinder/SearchDrug.jsx
@@ -0,0 +1,33 @@
+const SearchDrug = [
+    {
+      substance: "Ibuprofen",
+      drug: "Ibum Forte",
+    },
+    {
+        substance: "Ibuprofen",
+        drug: "Ibufen",
+      },
+    {
+        substance: "Ibuprofen",
+        drug: "Ibupropm",
+      },
+      
+      {
+        substance: "Ibuprofen",
+        drug: "Nurofen",
+      },
+      {
+        substance: "Paracetamol",
+        drug: "Paracetamol DOZ",
+      },
+      {
+        substance: "Paracetamol",
+        drug: "Pedicetamol",
+      },
+      {
+        substance: "Paracetamol",
+        drug: "Apap Junior",
+      },
+];
+  
+  export default SearchDrug;
