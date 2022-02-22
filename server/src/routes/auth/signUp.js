72767eeb4f1cbae7f9f27e58daa77e724e8336b3
Marcin Wos <marcin.wos@cdq.com>
2022-02-22 03:47:10 +0100

    added basic backend setup

---
+++ server/src/routes/auth/signUp.js
@@ -0,0 +1,12 @@
+// eslint-disable-next-line no-unused-vars
+import express from 'express';
+
+/**
+ * @param {express.Request} req
+ * @param {express.Response} res
+ */
+const signUp = (req, res) => {
+  res.send('Hello World!');
+};
+
+export default signUp;
