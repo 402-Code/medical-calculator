a6cbde278542db6ec8709be1936de073d18d42c5
Mateusz <matzaj101@gmail.com>
2022-02-25 11:15:44 +0100

    changes after review

--- server/src/routes/auth/signUp/signUp.js
+++ server/src/routes/auth/signUp/signUp.js
@@ -1,7 +1,6 @@
 import bcrypt from 'bcryptjs';
 import User from '../../../models';
 import { signupValidation } from './validateSignUp';
-import { StatusCodes } from 'http-status-codes';
 
 /**
  * @param {import('express').Request} req
@@ -13,14 +12,11 @@ const signUp = async (req, res) => {
 
   //validation
   const { error } = signupValidation(req.body);
-  if (error) return res.status(StatusCodes.BAD_REQUEST).send(error.details[0].message);
+  if (error) return res.send({ status: 'fail', message: error.details[0].message });
 
   // checking if this email is already in db
   const isEmailExists = await User.findOne({ email });
-  if (isEmailExists)
-    return res
-      .status(StatusCodes.BAD_REQUEST)
-      .send({ status: 'fail', message: `An account for email ${email} already exist` });
+  if (isEmailExists) return res.send({ status: 'fail', message: `An account for email ${email} already exist` });
 
   // hash the password
   const salt = await bcrypt.genSalt(10);
@@ -31,12 +27,12 @@ const signUp = async (req, res) => {
   try {
     const newUser = await user.save();
     const { username, email, createdAt, id } = newUser;
-    return res.status(StatusCodes.CREATED).send({
+    return res.send({
       status: 'success',
       user: { username, email, createdAt, id }
     });
   } catch (err) {
-    return res.status(StatusCodes.INTERNAL_SERVER_ERROR).send({ status: 'fail', message: err });
+    return res.send({ status: 'fail', message: 'Cannot create account for user' });
   }
 };
 
