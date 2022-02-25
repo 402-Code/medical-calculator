f61a1994b8c8b804fc91f68e8b67644db2fa11b6
Ewelina W <ewelina.wystepek@gmail.com>
2022-02-25 19:27:09 +0100

    Endpoint for current user added

--- server/src/routes/auth/index.js
+++ server/src/routes/auth/index.js
@@ -1,8 +1,18 @@
 import { Router } from 'express';
 import signUp from './signUp';
+import User from './../../models/user';
+import { StatusCodes } from 'http-status-codes';
 
 const authRouter = Router();
 
 authRouter.post('/sign-up', signUp);
+
+authRouter.get('/me', async (req, res) => {
+    //const user = req.user
+    const user = await User.findOne({ username: 'mateusz' }).exec();
+    if (!user) { res.status(StatusCodes.UNAUTHORIZED).send('User not logged in') };
+    const { username, email, createdAt } = user;
+    res.json({ username, email, createdAt });
+})
 
 export default authRouter;
