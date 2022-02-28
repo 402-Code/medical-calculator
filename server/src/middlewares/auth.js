c36d9cf64800084dde158bcda269ccc15b39c932
unknown <ty@example.comaleksandra.tarnawska@coderscrew.pl>
2022-02-28 01:49:57 +0100

    MIL-102Add_authentication_middleware

---
+++ server/src/middlewares/auth.js
@@ -0,0 +1,27 @@
+import express from 'express';
+import jwt from 'jsonwebtoken';
+import user from '../models';
+
+/**
+ * @param {import('express').Request} req
+ * @param {import('express').Response} res
+ */
+export function authMiddleware(req, res, next) {
+    try {
+        const accessToken = req.cookies['access-token']
+        console.log(accessToken);
+
+        const payload = jwt.verify(accessToken, process.env.SECRET_TOKEN)
+        console.log(payload);
+        req.user = { id: payload._id };
+
+        const { username, email, createdAt } = user;
+        req.json({ username, email, createdAt });
+     
+        return next();
+    }
+    catch (err) {
+        res.status(401).send({ message: 'Brak autoryzacji' })
+    }
+};
+
