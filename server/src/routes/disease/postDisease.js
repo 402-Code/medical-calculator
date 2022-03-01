4757557a1827ddf9d79134111d3c29eec8f7945a
Szymon Kwak <szymon.kwak@gmail.com>
2022-03-01 22:54:27 +0100

    clean unnecessary comments

--- server/src/routes/disease/postDisease.js
+++ server/src/routes/disease/postDisease.js
@@ -1,12 +1,9 @@
 import { StatusCodes } from 'http-status-codes';
-import User from '../../models/user';
 
 const postDisease = async (req, res) => {
   const disease = { eventLog: [], initialDrug: req.body.initialDrug };
 
   try {
-    // const user = await User.findById('621e8cb924b74f4dd1952d44');
-    // const kid = await user.kids.id('621e914f1f46dbe4c841b4e2');
     const kid = await req.user.kids.id(req.body.kidId);
     kid.diseases.push(disease);
 
