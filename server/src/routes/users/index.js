c21877571e20b22e15fe6c0ade6a1ce1d9a1645b
Mateusz <matzaj101@gmail.com>
2022-03-02 15:51:51 +0100

    update and delete kid for a user

--- server/src/routes/users/index.js
+++ server/src/routes/users/index.js
@@ -11,6 +11,7 @@ userRouter.get('/', async (req, res) => {
     res.status(500).send(error);
   }
 });
+
 userRouter.get('/:userId', async (req, res) => {
   try {
     const users = await User.findOne({ _id: req.params.userId });
@@ -19,6 +20,7 @@ userRouter.get('/:userId', async (req, res) => {
     res.status(500).send(error);
   }
 });
+
 userRouter.get('/:userId/kids', async (req, res) => {
   try {
     const users = await User.findOne({ _id: req.params.userId });
@@ -27,16 +29,66 @@ userRouter.get('/:userId/kids', async (req, res) => {
     res.status(500).send(error);
   }
 });
+
 userRouter.get('/:userId/kids/:kidId', async (req, res) => {
   const { userId, kidId } = req.params;
   try {
-    const users = await User.findOne({ _id: userId });
-    const kid = users.kids.filter((kid) => kid._id.toString() === kidId);
+    const user = await User.findOne({ _id: userId });
+    const kid = user.kids.filter((kid) => kid._id.toString() === kidId);
     res.status(200).send(kid);
   } catch (error) {
     console.log(error);
     res.status(500).send(error);
   }
 });
+
+userRouter.put('/:userId/kids/:kidId', async (req, res) => {
+  const { userId, kidId } = req.params;
+  try {
+    const user = await User.findOne({ _id: userId });
+    const kid = user.kids.find((kid) => kid._id.toString() === kidId);
+    propertyUpdate(kid, req.body);
+    await user.save();
+    res.status(200).send(kid);
+  } catch (error) {
+    console.log(error);
+    res.status(500).send(error);
+  }
+});
+
+userRouter.delete('/:userId/kids/:kidId', async (req, res) => {
+  const { userId, kidId } = req.params;
+  try {
+    const user = await User.findOne({ _id: userId });
+    const kidIndex = user.kids.findIndex((kid) => kid._id.toString() === kidId);
+    user.kids.splice(kidIndex, 1);
+    await user.save();
+    res.status(200).send(user);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+userRouter.post('/:userId/kids', async (req, res) => {
+  const { userId } = req.params;
+  try {
+    const kid = { ...req.body };
+    const user = await User.findOne({ _id: userId });
+    user.kids.push(kid);
+    await user.save();
+    res.status(200).send(user);
+  } catch (error) {
+    console.log(error);
+    res.status(500).send(error);
+  }
+});
+
+function propertyUpdate(kid, data) {
+  kid.name = data.name;
+  kid.dateOfBirth = data.dateOfBirth;
+  kid.height = data.height;
+  kid.weight = data.weight;
+  kid.gender = data.gender;
+}
 
 export default userRouter;
