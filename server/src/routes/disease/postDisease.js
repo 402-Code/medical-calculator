a8f974267ad9685044138cafc0a7a0f26adf5ee6
Szymon Kwak <szymon.kwak@gmail.com>
2022-03-03 09:22:47 +0100

    clean mocks

--- server/src/routes/disease/postDisease.js
+++ server/src/routes/disease/postDisease.js
@@ -1,14 +1,6 @@
 import { StatusCodes } from 'http-status-codes';
-import User from '../../models/user';
-import Drug from '../../models/drug';
 
 const postDisease = async (req, res) => {
-  // ----------------- mocks
-  req.body.initialDrug = await Drug.findById('621aa33d871734d3d6f6b960');
-  req.user = await User.findById('621e8cb924b74f4dd1952d44');
-  req.body.kidId = '621e914f1f46dbe4c841b4e2';
-  // ----------------- mocks
-
   try {
     const disease = {
       eventLog: [{ eventType: 'DrugApplication', drugId: req.body.initialDrug._id }],
