9f005f67a7e4490c40ecd43091bd46eaded4144d
Szymon Kwak <szymon.kwak@gmail.com>
2022-03-03 09:21:44 +0100

    post disease with mocks

--- server/src/routes/disease/postDisease.js
+++ server/src/routes/disease/postDisease.js
@@ -3,23 +3,22 @@ import User from '../../models/user';
 import Drug from '../../models/drug';
 
 const postDisease = async (req, res) => {
-  const drug = await Drug.findById('621aa33d871734d3d6f6b960');
-  const disease = {
-    // eventLog: [{ drugId: req.body.initialDrug }],
-    // initialDrug: req.body.initialDrug
-    eventLog: [{ eventType: 'SymptomSpotted', drugId: drug._id, symptoms: ['one', 'two'] }],
-    initialDrug: drug._id
-  };
+  // ----------------- mocks
+  req.body.initialDrug = await Drug.findById('621aa33d871734d3d6f6b960');
+  req.user = await User.findById('621e8cb924b74f4dd1952d44');
+  req.body.kidId = '621e914f1f46dbe4c841b4e2';
+  // ----------------- mocks
 
   try {
-    const user = await User.findById('621e8cb924b74f4dd1952d44');
-    const kid = await user.kids.id('621e914f1f46dbe4c841b4e2');
+    const disease = {
+      eventLog: [{ eventType: 'DrugApplication', drugId: req.body.initialDrug._id }],
+      initialDrug: req.body.initialDrug._id
+    };
 
-    // const kid = await req.user.kids.id(req.body.kidId);
+    const kid = await req.user.kids.id(req.body.kidId);
     kid.diseases.push(disease);
 
-    // await req.user.save();
-    await user.save();
+    await req.user.save();
     res.status(StatusCodes.CREATED).send(kid);
   } catch (err) {
     res.status(StatusCodes.BAD_REQUEST).send({ message: err.message });
