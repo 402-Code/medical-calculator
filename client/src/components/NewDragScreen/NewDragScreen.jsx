40c764ead110923d63bb3ed4d715f7d76acdd9a3
Mateusz <matzaj101@gmail.com>
2022-03-17 21:54:24 +0100

    remove clg

--- client/src/components/NewDragScreen/NewDragScreen.jsx
+++ client/src/components/NewDragScreen/NewDragScreen.jsx
@@ -6,27 +6,19 @@ import Profile from '../Profile/Profile';
 import { ChildContext } from '../../context/ChildContext';
 
 const NewDragScreen = ({ setSelectedDrug }) => {
-  console.log('setselecteddrug', setSelectedDrug);
-
   const [activeKid, setActiveKid] = useState({});
   const ctx = useContext(ChildContext);
 
   useEffect(() => {
-    if (ctx.user.kids?.length > 0) {
-      const firstKid = ctx.user.kids[0];
-      console.log('firstKid', firstKid);
-      setActiveKid(firstKid);
-      console.log('activekid1', activeKid);
-    }
+    const firstKid = ctx.user.kids[0];
+    setActiveKid(firstKid);
   }, []);
 
   console.log('activekid2', activeKid);
 
   if (ctx.user.kids !== null) {
-    console.log('!@#$%^&*()_+', ctx.user);
     return (
       <>
-        {console.log('activekid3', activeKid)}
         <KidSelect activeKid={ctx.user.kids[0]} setActiveKid={setActiveKid} />
         <SelectDrug setSelectedDrug={setSelectedDrug} activeKid={ctx.user.kids[0]} />
       </>
